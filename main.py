b5fb87e77de1d8bc9e61bc62ab2df5fb457314f3
Alex Davis <8m3xlt@gmail.com>
2020-11-14 22:54:51 +0000

    Moved code to a function

--- main.py
+++ main.py
@@ -14,76 +14,82 @@ from sqlite3 import Error
 #           used to store images and files (BYTES)         #
 ############################################################
 
-conn = sqlite3.connect("bookings.db") #create a connection and specify the database it should connect to
-#if that database doesn't exist, it creates it.
-cursor = conn.cursor() #create a cursor object using the connection object returned by the connect method
-print ("Successfully connected to database")
 
-while True:
-    date = input("please enter a date: ")
+def write_data(date: str, rider_firstname: str, rider_surname: str, email, instructor: str, lessontype: str, horse: str, bookings: int, arena: str) -> bool:
+    conn = sqlite3.connect("bookings.db")  # create a connection and specify the database it should connect to
+    # if that database doesn't exist, it creates it.
+    cursor = conn.cursor()  # create a cursor object using the connection object returned by the connect method
+    print("Successfully connected to database")
 
-    rider_firstname = input("Please enter the rider's first name: ").title()
+    try:
+        sqlite_create_table_query = '''CREATE TABLE if not exists booking(
+        date REAL NOT NULL,
+        rider_firstname TEXT NOT NULL,
+        rider_surname TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        instructor TEXT NOT NULL,
+        lessontype TEXT NOT NULL,
+        horsename TEXT NOT NULL,
+        bookings INT NOT NULL,
+        arena TEXT NOT NULL);''' #prepares a 'create table' query with columns
+        #date format dd.mm.yyyy e.g. 02.11.2009 --- Doesn't work
+        cursor.execute(sqlite_create_table_query) #creates the table
+        conn.commit()
+        print("sqlite table created")
 
-    rider_surname = input("Please enter the rider's surname: ").title()
+        sqlite_insert_query = '''INSERT INTO booking
+        (date,rider_firstname,rider_surname,email,instructor,lessontype,horsename,bookings,arena)
+        VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+        table_row = (date, rider_firstname, rider_surname, email, instructor, lessontype, horse, bookings, arena)
+        count = cursor.execute(sqlite_insert_query, table_row)
+        conn.commit()
+        print ("Information recorded", cursor.rowcount)
+        cursor.close() #closes the cursor
 
-    email = input("Please enter the email address of the rider or rider's guardian: ")
-    
-    instructor = input("Please enter the instructor's name: ").title()
-    
-    lessontype = input("Please enter what type of lesson it is: ")
-    
-    horse = input("Please enter the horse's name: ").title()
-    
-    bookings = int(input ("How many bookings are there: "))
-    
-    arena = input("What arena is it in: ").title()
+    except sqlite3.Error as error: #catch database error
+        print("error:", error) #print error
+        return False
 
-    print("\nThe data you inputted was: ")
-    print("\nDate: " + date)
+    finally:
+        if conn:
+            conn.close() #close SQLite database
+            print("The SQLite connection is closed")
+    return True
 
-    print("\nThe rider's first name: " + rider_firstname)
-    print("\nThe rider's surname: " + rider_surname)
-    print("\nThe email of the rider or rider's guardian: " + email)
-    print("\nThe lesson instructor: " + instructor)
-    print("\nThe lesson type" + lessontype)
-    print("\nThe horse's name: " + horse)
-    print("\nThe numeber of bookings: " + str(bookings))
-    print("\nThe lesson arena: " + arena)
+if __name__ == '__main__':
+    while True:
+        date = input("please enter a date: ")
 
-    check = input("\nIs this correct? y/n ").lower()
-    if check in ["y", "yes"]:
-        break
+        rider_firstname = input("Please enter the rider's first name: ").title()
 
-try:
-    sqlite_create_table_query = '''CREATE TABLE if not exists booking(
-    date REAL NOT NULL,
-    rider_firstname TEXT NOT NULL,
-    rider_surname TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    instructor TEXT NOT NULL,
-    lessontype TEXT NOT NULL,
-    horsename TEXT NOT NULL,
-    bookings INT NOT NULL,
-    arena TEXT NOT NULL);''' #prepares a 'create table' query with columns
-    #date format dd.mm.yyyy e.g. 02.11.2009 --- Doesn't work
-    cursor.execute(sqlite_create_table_query) #creates the table
-    conn.commit() 
-    print ("sqlite table created")
+        rider_surname = input("Please enter the rider's surname: ").title()
 
-    sqlite_insert_query = '''INSERT INTO booking
-    (date,rider_firstname,rider_surname,email,instructor,lessontype,horsename,bookings,arena)
-    VALUES
-    (?, ?, ?, ?, ?, ?, ?, ?, ?)'''
-    table_row = (date, rider_firstname, rider_surname, email, instructor, lessontype, horse, bookings, arena)
-    count = cursor.execute(sqlite_insert_query, table_row)
-    conn.commit()
-    print ("Information recorded", cursor.rowcount)
-    cursor.close() #closes the cursor 
+        email = input("Please enter the email address of the rider or rider's guardian: ")
 
-except sqlite3.Error as error: #catch database error
-    print ("error:", error) #print error
+        instructor = input("Please enter the instructor's name: ").title()
 
-finally:
-    if (conn):
-        conn.close() #close SQLite database
-        print ("The SQLite connection is closed")
+        lessontype = input("Please enter what type of lesson it is: ")
+
+        horse = input("Please enter the horse's name: ").title()
+
+        bookings = int(input("How many bookings are there: "))
+
+        arena = input("What arena is it in: ").title()
+
+        print("\nThe data you inputted was: ")
+        print("\nDate: " + date)
+
+        print("\nThe rider's first name: " + rider_firstname)
+        print("\nThe rider's surname: " + rider_surname)
+        print("\nThe email of the rider or rider's guardian: " + email)
+        print("\nThe lesson instructor: " + instructor)
+        print("\nThe lesson type" + lessontype)
+        print("\nThe horse's name: " + horse)
+        print("\nThe numeber of bookings: " + str(bookings))
+        print("\nThe lesson arena: " + arena)
+
+        check = input("\nIs this correct? y/n ").lower()
+        if check in ["y", "yes"]:
+            break
+    write_data(date, rider_firstname, rider_surname, email, instructor, lessontype, horse, bookings, arena)
